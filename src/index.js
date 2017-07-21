8cfaf005531cfc37628035ebdb1783478a2d434d
Tony Pinkevich <tony@untimestudio.com>
2017-07-21 17:58:32 +0300

    fixed tabs in index.js

--- src/index.js
+++ src/index.js
@@ -23,94 +23,95 @@ const subpshapesToDifferentIcon = require('./icons/subshapesToDifferent.png');
 const untimeLogoIcon = require('./icons/untime.png');
 
 const myScriptBuildUI = (thisObj) => {
-	const myPanel = (thisObj instanceof Panel) ?
-		thisObj :
-		new Window('palette', 'Untime Toolbox', undefined);
+  const myPanel = (thisObj instanceof Panel) ?
+    thisObj :
+    new Window('palette', 'Untime Toolbox', undefined);
 
-	// set groups >
-	const mainGrp = myPanel.add('group', undefined);
-	mainGrp.orientation = settings.general.orientation;
-	mainGrp.alignment = ['left', 'top'];
-	mainGrp.alignChildren = ['left', 'top'];
+  // set groups >
+  const mainGrp = myPanel.add('group', undefined);
+  mainGrp.orientation = settings.general.orientation;
+  mainGrp.alignment = ['left', 'top'];
+  mainGrp.alignChildren = ['left', 'top'];
 
-	const groupOne = mainGrp.add('group', undefined);
-	groupOne.orientation = 'row';
+  const groupOne = mainGrp.add('group', undefined);
+  groupOne.orientation = 'row';
 
-	const separator01 = mainGrp.add('group', settings.general.separatorSize);
-	changeBGcolor(separator01, [0, 0, 0, 0.2]);
+  const separator01 = mainGrp.add('group', settings.general.separatorSize);
+  changeBGcolor(separator01, [0, 0, 0, 0.2]);
 
-	const groupTwo = mainGrp.add('group', undefined);
-	groupTwo.orientation = 'row';
+  const groupTwo = mainGrp.add('group', undefined);
+  groupTwo.orientation = 'row';
 
-	const separator02 = mainGrp.add('group', settings.general.separatorSize);
-	changeBGcolor(separator02, [0, 0, 0, 0.2]);
+  const separator02 = mainGrp.add('group', settings.general.separatorSize);
+  changeBGcolor(separator02, [0, 0, 0, 0.2]);
 
-	const groupThree = mainGrp.add('group', undefined);
-	groupThree.orientation = 'row';
-	// set groups  <
-	// set buttons >
-	const createNullButton = groupOne.add('iconbutton', [0, 0, 24, 24], loadImage(createNullIcon), { style: 'toolbutton', toggle: false });
-	createNullButton.size = settings.general.buttonsSize;
-	createNullButton.onClick = createNull;
-	createNullButton.helpTip = 'Create a Null';
+  const groupThree = mainGrp.add('group', undefined);
+  groupThree.orientation = 'row';
+  // set groups  <
+  // set buttons >
+  const createNullButton = groupOne.add('iconbutton', [0, 0, 24, 24], loadImage(createNullIcon), { style: 'toolbutton', toggle: false });
+  createNullButton.size = settings.general.buttonsSize;
+  createNullButton.onClick = createNull;
+  createNullButton.helpTip = 'Create a Null';
 
-	onionSkinButton = groupOne.add('iconbutton', [0, 0, 24, 24], loadImage(onionSkinIcon), { style: 'toolbutton', toggle: false });
-	onionSkinButton.size = settings.general.buttonsSize;
-	onionSkinButton.onClick = launchOnionSkin;
-	onionSkinButton.helpTip = 'Enable or Disable Onion Skin';
+  const onionSkinButton = groupOne.add('iconbutton', [0, 0, 24, 24], loadImage(onionSkinIcon), { style: 'toolbutton', toggle: false });
+  onionSkinButton.size = settings.general.buttonsSize;
+  onionSkinButton.onClick = launchOnionSkin;
+  onionSkinButton.helpTip = 'Enable or Disable Onion Skin';
 
-	createFoldersButton = groupOne.add('iconbutton', [0, 0, 24, 24], loadImage(addFoldersIcon), { style: 'toolbutton', toggle: false });
-	createFoldersButton.size = settings.general.buttonsSize;
-	createFoldersButton.onClick = createFolders;
-	createFoldersButton.helpTip = 'Create default Folders';
+  const createFoldersButton = groupOne.add('iconbutton', [0, 0, 24, 24], loadImage(addFoldersIcon), { style: 'toolbutton', toggle: false });
+  createFoldersButton.size = settings.general.buttonsSize;
+  createFoldersButton.onClick = createFolders;
+  createFoldersButton.helpTip = 'Create default Folders';
 
-	setSameLabelColorsButton = groupOne.add('iconbutton', [0, 0, 24, 24], loadImage(fillFoldersIcon), { style: 'toolbutton', toggle: false });
-	setSameLabelColorsButton.size = settings.general.buttonsSize;
-	setSameLabelColorsButton.onClick = setSameLabelColors;
-	setSameLabelColorsButton.helpTip = 'Change label colors of Folders';
+  const setSameLabelColorsButton = groupOne.add('iconbutton', [0, 0, 24, 24], loadImage(fillFoldersIcon), { style: 'toolbutton', toggle: false });
+  setSameLabelColorsButton.size = settings.general.buttonsSize;
+  setSameLabelColorsButton.onClick = setSameLabelColors;
+  setSameLabelColorsButton.helpTip = 'Change label colors of Folders';
 
-	clearShapeButton = groupTwo.add('iconbutton', [0, 0, 24, 24], loadImage(clearShapeIcon), { style: 'toolbutton', toggle: false });
-	clearShapeButton.size = settings.general.buttonsSize;
-	clearShapeButton.onClick = clearShape;
-	clearShapeButton.helpTip = 'Remove additional groups from shapes after \'Create Shape Layers\' command';
+  const clearShapeButton = groupTwo.add('iconbutton', [0, 0, 24, 24], loadImage(clearShapeIcon), { style: 'toolbutton', toggle: false });
+  clearShapeButton.size = settings.general.buttonsSize;
+  clearShapeButton.onClick = clearShape;
+  clearShapeButton.helpTip = 'Remove additional groups from shapes after \'Create Shape Layers\' command';
 
-	separateShapesSingleLayerButton = groupTwo.add('iconbutton', [0, 0, 24, 24], loadImage(subpshapesToSingleIcon), { style: 'toolbutton', toggle: false });
-	separateShapesSingleLayerButton.size = settings.general.buttonsSize;
-	separateShapesSingleLayerButton.onClick = separateShapesSingleLayer;
-	separateShapesSingleLayerButton.helpTip = 'Subshapes to single layer';
+  const separateShapesSingleLayerButton = groupTwo.add('iconbutton', [0, 0, 24, 24], loadImage(subpshapesToSingleIcon), { style: 'toolbutton', toggle: false });
+  separateShapesSingleLayerButton.size = settings.general.buttonsSize;
+  separateShapesSingleLayerButton.onClick = separateShapesSingleLayer;
+  separateShapesSingleLayerButton.helpTip = 'Subshapes to single layer';
 
-	separateShapesDifferentLayersButton = groupTwo.add('iconbutton', [0, 0, 24, 24], loadImage(subpshapesToDifferentIcon), { style: 'toolbutton', toggle: false });
-	separateShapesDifferentLayersButton.size = settings.general.buttonsSize;
-	separateShapesDifferentLayersButton.onClick = separateShapesDifferentLayers;
-	separateShapesDifferentLayersButton.helpTip = 'Subshapes to different layers';
+  const separateShapesDifferentLayersButton = groupTwo.add('iconbutton', [0, 0, 24, 24], loadImage(subpshapesToDifferentIcon), { style: 'toolbutton', toggle: false });
+  separateShapesDifferentLayersButton.size = settings.general.buttonsSize;
+  separateShapesDifferentLayersButton.onClick = separateShapesDifferentLayers;
+  separateShapesDifferentLayersButton.helpTip = 'Subshapes to different layers';
 
-	creaditsButton = groupThree.add('iconbutton', [0, 0, 18, 18], loadImage(untimeLogoIcon), { style: 'toolbutton', toggle: false });
-	creaditsButton.size = settings.general.buttonsSize;
-	creaditsButton.onClick = () => openUrl('https://untimestudio.com');
-	creaditsButton.helpTip = 'Untime Studio';
+  const creaditsButton = groupThree.add('iconbutton', [0, 0, 18, 18], loadImage(untimeLogoIcon), { style: 'toolbutton', toggle: false });
+  creaditsButton.size = settings.general.buttonsSize;
+  creaditsButton.onClick = () => openUrl('https://untimestudio.com');
+  creaditsButton.helpTip = 'Untime Studio';
 
-	helpButton = groupThree.add('button', undefined, '?');
-	helpButton.size = settings.general.buttonsSize;
-	helpButton.onClick = () => openUrl('https://github.com/tonypinkevych/untime-toolbox');
-	helpButton.helpTip = 'Help';
-	// set buttons <
+  const helpButton = groupThree.add('button', undefined, '?');
+  helpButton.size = settings.general.buttonsSize;
+  helpButton.onClick = () => openUrl('https://github.com/tonypinkevych/untime-toolbox');
+  helpButton.helpTip = 'Help';
+  // set buttons <
 
-	// Setup panel sizing and make panel resizable
-	myPanel.layout.layout(true);
-	myPanel.minimumSize = mainGrp.size;
-	myPanel.layout.resize();
-	myPanel.onResizing = myPanel.onResize = () => thisObj.layout.resize();
+  // Setup panel sizing and make panel resizable
+  myPanel.layout.layout(true);
+  myPanel.minimumSize = mainGrp.size;
+  myPanel.layout.resize();
+  myPanel.onResizing = thisObj.layout.resize();
+  myPanel.onResize = () => thisObj.layout.resize();
 
-	return myPanel;
+  return myPanel;
 };
 
 const myScript = (thisObj) => {
-	const myScriptPal = myScriptBuildUI(thisObj);
+  const myScriptPal = myScriptBuildUI(thisObj);
 
-	if ((myScriptPal != null) && (myScriptPal instanceof Window)) {
-		myScriptPal.center();
-		myScriptPal.show();
-	}
+  if ((myScriptPal != null) && (myScriptPal instanceof Window)) {
+    myScriptPal.center();
+    myScriptPal.show();
+  }
 };
 
 // add script to global variable
